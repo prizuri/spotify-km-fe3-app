6f24f36b91dfc03753390584074e96c34822e950
Prizuri Hartadi <prizurihartadi@gmail.com>
2022-03-23 06:57:06 +0700

    Make a change to make interface as homework guidline module 2 session 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import data from "./data"
 
+const imageEl = data.album.images[0].url
+const titleEl = data.name
+const artistEl = data.album.artists[0].name
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <img src={imageEl} className="App-logo" alt="bohemian album"/>
+      <h1>{titleEl}</h1>
+      <p>{artistEl}</p>
+      <button>Select</button>
     </div>
   );
 }
