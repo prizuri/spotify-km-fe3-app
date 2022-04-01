398e8e6a770895623e63e2fd053856b7245b81a9
Prizuri Hartadi <61555718+prizuri@users.noreply.github.com>
2022-04-01 15:30:30 +0700

    trying to make spotify authorization

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import data from './components/data-manager/data';
 import Header from './components/header';
 import ImageSong from './components/image';
 import Paragraph from './components/paragraph';
-import axios from "axios"
+// import axios from "axios"
 import React from "react"
 
 function App() {
@@ -19,18 +19,27 @@ function App() {
   // })
   const [query, setQuery] = React.useState("")
   const [dataSpotify, setDataSpotify] = React.useState([])
+  const [token, setToken]=React.useState("")
 
-  React.useEffect(() => {
-    axios.get("")
-      .then(response => console.log(response.data))
-  }, [])
+  // React.useEffect(() => {
+
+  //   axios.get("")
+  //     .then(response => console.log(response.data))
+  // }, [])
   function handleInput(event) {
     setQuery(event.target.value)
   }
 
+  function requestUserAuthorization(){
+    const client_id=process.env.REACT_APP_SPOTIFY_CLIENT_ID
+    const redirect_uri="https://localhost:3000"
+    const scope="playlist-modify-private"
+    const url=`https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&scope=${scope}&redirect_uri=${redirect_uri}`
+  }
   return (
     <div className="App" >
       {/* {getData} */}
+      <button onClick={requestUserAuthorization}>Login</button>
       <form>
         <input type="text" onChange={handleInput} />
         <button>Select</button>
